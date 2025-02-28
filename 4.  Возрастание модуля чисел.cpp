df0155c3fd3fee372ee55c8137ea873e421807f5
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-28 11:07:51 +0300

    Add files via upload

---
+++ 4.  Возрастание модуля чисел.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+void printSorted(const std::vector<int>& vec) {
+    size_t pos = 0;  //мето первого неотрицательного элемента
+
+    //Поиск первого неотрицательного элемента
+    while (pos < vec.size() && vec[pos] < 0) {
+        pos++;
+    }
+    int left = pos - 1;
+    int right = pos;
+
+    while (left >= 0 || right < vec.size()) { //печать элементов с первого неотрицательного
+        if (left < 0) {
+            std::cout << vec[right++] << ' ';
+        }
+        else if (right >= vec.size()) {
+            std::cout << vec[left--] << ' ';
+        }
+        else {
+            if (abs(vec[left]) <= vec[right]) {
+                std::cout << vec[left--] << ' ';
+            }
+            else {
+                std::cout << vec[right++] << ' ';
+            }
+        }
+    }
+}
+
+int main() {
+    std::vector<int> vec = { -100, -50, -5, 1, 10, 15 };
+
+    printSorted(vec);
+
+    return 0;
+}
